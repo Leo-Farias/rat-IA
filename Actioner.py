a3f0ddc1ff7196a327b4842ff4d5662d8551fff0
leonardo-farias <leonnardo.bf@gmail.com>
2019-04-02 17:22:40 -0300

    Added more mechanic to the rat moviment, still bugged.

--- Actioner.py
+++ Actioner.py
@@ -11,25 +11,107 @@ class Rat:
 		self.y = y
 		self.position = [self.x, self.y]
 		self.last_position = []
+		self.direction = "Right"
 
-	def update_coordenates(self, x, y):
-		self.x = x
-		self.y = y
+	def set_bifurcation(self, look, around):
+		'''
+		Method that will see the avaible paths for the rat
+		look is a tuple that will show where to look for up/down = (0,2) or right/left = (1,3)
+		'''
+
+		if around[look[0]] != 1 and around[look[1]] == 1:
+			possibilities = (True, False) 
+		
+		elif around[look[0]] == 1 and around[look[1]] != 1:
+			possibilities = (False,True)  
+		
+		elif around[look[0]] != 1 and around[look[1]] !=1:
+			possibilities = (True, True)
+		
+		else:
+			possibilities = (False, False) 
+
+		return possibilities
+
+
+	def go_up(self):
+		'''
+		Moves the rat one position above.
+		'''
+		self.y = self.y - 1
+
+	def go_right(self):
+		'''
+		Moves the rat one position right.
+		'''
+		self.x = self.x + 1
+
+	def go_down(self):
+		'''
+		Moves the rat one position down.
+		'''
+		self.y = self.y + 1
+
+	def go_left(self):
+		'''
+		Moves the rat one position left.
+		'''
+		self.x = self.x - 1
 
 	def walk(self, around):
-		choice = "default"
-		moviments = {"Up": [self.y - 1, self.x],
-						"Right": [self.y, self.x + 1],
-						"Down": [self.y + 1, self.x],
-						"Left": [self.y, self.x - 1]}
+		'''
+		This method is reponsible for the rat moviment.
+		It receives a list of the spaces close to it by one positon up, right, down and left.
+		The logic is this: the rat will follow a direction untill it hits
+		a wall. If it hits a wall it will look the the other direction
+		for example: It walks all the way to the right, it finds a wall then it will
+		look up and down.
+		In case it finds a dead end it will go back from where he came from and search for 
+		places he went straight but had an entrance.
+		'''
+		moviments = {"Up": self.go_up,
+						"Right": self.go_right,
+						"Down": self.go_down,
+						"Left": self.go_left}
 		options = ["Up", "Right", "Down", "Left"]
-		counter = 0
 		change_direction = False
-		
-		for space in around:
+		node = False
+		bifurc = (False, False)# It will check if the bifurcations are avaible, right/left or up/down
+		dir_as_num = options.index(self.direction) # Direction as number. This will give us the position in the list of the direction
+
+		# this if statement will tell if the rat found a wall in its straight walk.
+		# the index function is used to get the position of the direction on the list
+		# and then check the same position for the around list.
+		if around[dir_as_num] not in [0,2,4]:
+			print("Hey")
+			
+			bifurc = self.set_bifurcation((0,2), around) if dir_as_num %2 == 0 else self.set_bifurcation((1,3), around)
+			
+			if bifurc == (True, True):
+				node = True
+				dir_as_num -= 1
+				self.direction = options[dir_as_num]
+
+			elif bifurc == (False, False):
+				self.find_node()
+
+			else:
+				# making this change so we dont get an out of range error
+				if dir_as_num == 3:
+					dir_as_num = 1
+
+				# Making sure the rat will choose the avaible path.
+				if bifurc[0] == True:
+					dir_as_num -= 1
+				else:
+					dir_as_num += 1
+
+				self.direction = options[dir_as_num]
+
 			
 
 		#If the choice remains as "default" than it means that the rat found a dead end.
 
-		self.position = moviments[choice]
-		self.update_coordenates(self.position[1], self.position[0])	
+
+		moviments[self.direction]()
+		self.position = [self.y, self.x]

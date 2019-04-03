80faecbcfcd8e5b3fc93f7521f7d89b997181fd0
leonardo-farias <leonnardo.bf@gmail.com>
2019-04-03 17:39:43 -0300

    Updated more mechanics to the rat walk, atm it is very bugged

--- Actioner.py
+++ Actioner.py
@@ -10,29 +10,11 @@ class Rat:
 		self.x = x
 		self.y = y
 		self.position = [self.x, self.y]
-		self.last_position = []
+		self.dp_location = []
 		self.direction = "Right"
-
-	def set_bifurcation(self, look, around):
-		'''
-		Method that will see the avaible paths for the rat
-		look is a tuple that will show where to look for up/down = (0,2) or right/left = (1,3)
-		'''
-
-		if around[look[0]] != 1 and around[look[1]] == 1:
-			possibilities = (True, False) 
-		
-		elif around[look[0]] == 1 and around[look[1]] != 1:
-			possibilities = (False,True)  
-		
-		elif around[look[0]] != 1 and around[look[1]] !=1:
-			possibilities = (True, True)
-		
-		else:
-			possibilities = (False, False) 
-
-		return possibilities
-
+		self.step_counter = 0
+		self.searching_bifurc = False
+		self.steps_to_dest = 0
 
 	def go_up(self):
 		'''
@@ -58,6 +40,62 @@ class Rat:
 		'''
 		self.x = self.x - 1
 
+	def set_bifurcation(self, look, around):
+		'''
+		Method that will see the avaible paths for the rat
+		look is a tuple that will show where to look for up/down = (0,2) or right/left = (1,3)
+		'''
+
+		if around[look[0]] != 1 and around[look[1]] == 1:
+			possibilities = (True, False) 
+		
+		elif around[look[0]] == 1 and around[look[1]] != 1:
+			possibilities = (False,True)  
+		
+		elif around[look[0]] != 1 and around[look[1]] !=1:
+			possibilities = (True, True)
+		
+		else:
+			possibilities = (False, False) 
+
+		return possibilities
+
+	def go_to_bifurc(self, options):
+		
+		print("Dead End. Going to Bifurc.")
+		destination = self.dp_location.pop()
+		step = destination[0]
+		d_to_d = destination[1]
+		self.steps_to_dest = self.step_counter - step
+		self.searching_bifurc = True
+		direction = d_to_d + 2 if d_to_d < 2 else d_to_d - 2
+
+		return options[direction]
+
+	def make_decision(self, bifurc, options, dir_as_num, back = False):
+		
+		if bifurc == (False, False) or back == True:
+			return self.go_to_bifurc(options)
+
+		elif bifurc == (True, True):
+			
+			dir_as_num -= 1
+			return  options[dir_as_num]
+
+		else:
+			# making this change so we dont get an out of range error
+			if dir_as_num == 3:
+				dir_as_num = 1
+
+			# Making sure the rat will choose the avaible path.
+			if bifurc[0] == True:
+				dir_as_num -= 1
+			else:
+				dir_as_num += 1
+
+			return options[dir_as_num]
+
+
 	def walk(self, around):
 		'''
 		This method is reponsible for the rat moviment.
@@ -69,51 +107,41 @@ class Rat:
 		In case it finds a dead end it will go back from where he came from and search for 
 		places he went straight but had an entrance.
 		'''
+
 		moviments = {"Up": self.go_up,
 						"Right": self.go_right,
 						"Down": self.go_down,
 						"Left": self.go_left}
-		options = ["Up", "Right", "Down", "Left"]
-		change_direction = False
-		node = False
-		bifurc = (False, False)# It will check if the bifurcations are avaible, right/left or up/down
-		dir_as_num = options.index(self.direction) # Direction as number. This will give us the position in the list of the direction
-
-		print(dir_as_num %2 == 0)
+		options = list(moviments.keys())
+		# It will check if the bifurcations are avaible, right/left or up/down
+		bifurc = (False, False)
+		
+		# Direction as number. This will give us the position in the list of the direction
+		dir_as_num = options.index(self.direction) 
 		bifurc = self.set_bifurcation((1,3), around) if dir_as_num %2 == 0 else self.set_bifurcation((0,2), around)
-		# this if statement will tell if the rat found a wall in its straight walk.
-		# the index function is used to get the position of the direction on the list
-		# and then check the same position for the around list.
-		if around[dir_as_num] not in [0,2,4]:
-			print("Hey")
-			
-			if bifurc == (True, True):
-				node = True
-				dir_as_num -= 1
-				self.direction = options[dir_as_num]
 
-			elif bifurc == (False, False):
-				self.find_node()
-
-			else:
-				# making this change so we dont get an out of range error
-				if dir_as_num == 3:
-					dir_as_num = 1
-
-				# Making sure the rat will choose the avaible path.
-				if bifurc[0] == True:
-					dir_as_num -= 1
+		if self.searching_bifurc == True:
+			self.steps_to_dest -= 1
+			if self.steps_to_dest == 0:
+				print("Found Bifurc")
+				if around.count(0) == 0:
+					print("Goin to next bifurc.")
+					self.dp_location.pop()
+					self.direction = self.make_decision(bifurc, options, dir_as_num, back = True)
 				else:
-					dir_as_num += 1
+					self.direction = self.make_decision(bifurc, options, dir_as_num)
+				self.searching_bifurc = False
 
-				self.direction = options[dir_as_num]
+		# this if statement will tell if the rat found a wall in its straight walk.
+		elif around[dir_as_num] not in [0,2,4]:
+			print("Collision")
+			self.direction = self.make_decision(bifurc, options, dir_as_num)
 
-			
-
-		#If the choice remains as "default" than it means that the rat found a dead end.
-
+		if bifurc != (False,False):
+			self.dp_location.append((self.step_counter, dir_as_num))
 
 		moviments[self.direction]()
-		print(self.direction)
-		print(bifurc)
 		self.position = [self.y, self.x]
+		self.step_counter = self.step_counter + 1 if self.searching_bifurc != True else self.step_counter - 1
+		print(self.dp_location)
+
